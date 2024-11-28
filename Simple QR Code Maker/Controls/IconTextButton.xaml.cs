dec8119c825bdff905ff8a07a08f6e7ee8e301c2
Matt Lacey <matt@mrlacey.co.uk>
2024-11-28 11:12:17 +0000

    simple custom controls for buttons with icons

---
+++ Simple QR Code Maker/Controls/IconTextButton.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+namespace Simple_QR_Code_Maker.Controls;
+
+public sealed partial class IconTextButton : Button
+{
+    public IconTextButton()
+    {
+        this.InitializeComponent();
+    }
+
+    public string Text
+    {
+        get { return (string)GetValue(TextProperty); }
+        set { SetValue(TextProperty, value); }
+    }
+
+    // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty TextProperty =
+        DependencyProperty.Register("Text", typeof(string), typeof(IconTextButton), new PropertyMetadata(null));
+
+    public UIElement Icon
+    {
+        get { return (UIElement)GetValue(IconProperty); }
+        set { SetValue(IconProperty, value); }
+    }
+
+    // Using a DependencyProperty as the backing store for Icon.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty IconProperty =
+        DependencyProperty.Register("Icon", typeof(UIElement), typeof(IconTextButton), new PropertyMetadata(null));
+
+}
--- Simple QR Code Maker/Controls/TextBorder.xaml.cs
+++ Simple QR Code Maker/Controls/TextBorder.xaml.cs
@@ -1,5 +1,6 @@
-using Microsoft.UI.Xaml;
+﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
 using Simple_QR_Code_Maker.Models;
 
 namespace Simple_QR_Code_Maker.Controls;
@@ -20,5 +21,41 @@ public sealed partial class TextBorder : UserControl
     private void Button_Click(object sender, RoutedEventArgs e)
     {
         TextBorder_Click?.Invoke(this, e);
+    }
+}
+
+// SymbolIcon is sealed 😢 so we need to wrap things in a UserControl
+public partial class SettingsSymbol : UserControl
+{
+    public SettingsSymbol()
+    {
+        this.Content = new SymbolIcon(Symbol.Setting);
+    }
+}
+
+// FontIcon isn't sealed 🎉
+public partial class ScanIcon : FontIcon
+{
+    public ScanIcon()
+    {
+        this.FontFamily = (FontFamily)Application.Current.Resources["SymbolThemeFontFamily"];
+        this.Glyph = "\uEE6F";
+    }
+}
+
+// ViewBox is sealed, so we need a UserControl
+public partial class ScanSymbol : UserControl
+{
+    public ScanSymbol()
+    {
+        this.Content = new Viewbox
+        {
+            Height = 16,
+            Child = new FontIcon
+            {
+                FontFamily = (FontFamily)Application.Current.Resources["SymbolThemeFontFamily"],
+                Glyph = "\uEE6F",
+            }
+        };
     }
 }
